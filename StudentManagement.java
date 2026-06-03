9da9ab58b46f0288d21af9694b673f8376950e5a
yashwanth <yashwanth.d52917@gmail.com>
2026-06-03 17:18:01 +0530

    tested whole project using test cases

--- StudentManagement.java
+++ StudentManagement.java
@@ -4,119 +4,184 @@ public class StudentManagement {
     public static void main(String[] args) {
         // creating scanner object ...
         Scanner sc = new Scanner(System.in);
-
+        // !!! --ARRAY OF OBJECT (USED TO ACCESS OBJECTS USING ARRAYS)
         Student[] students = new Student[100];
+        int id;
+        String name;
+        int marks;
         int count = 0;
         int accessing_while;
-        boolean found = false;
+        boolean found;
+        System.out.println("---------STUDENT MANAGEMENT SYSTEM---------");
         do {
+            System.out.println("LIST OF OPERATION :");
             System.out.println("ENTER 1 TO ADD STUDENT ");
             System.out.println("ENTER 2 TO ALL DISPLAY STUDENTS ");
             System.out.println("ENTER 3 TO SEARCH STUDENT BY ID ");
             System.out.println("ENTER 4 TO UPDATE STUDENT DETAILS ");
             System.out.println("ENTER 5 TO DELETE STUDENT ");
             int expression = sc.nextInt();
-            try {
+            // try {
 
-                switch (expression) {
-                    case 1:
-                        // adding the students ...
-                        // creating object inside switch because where we want add
-                        // add new Student the object should be updated
-                        // example : students[0]=s
-                        // entering second student students[1]=s//but here s acts as s1 ..
-                        Student s = new Student();
-                        // user Input
-                        System.out.println("enter the id of the student :");
-                        int id = sc.nextInt();
+            switch (expression) {
+                case 1:
+                    // adding the students ...
+                    // creating object inside switch because where we want add
+                    // add new Student the object should be updated
+                    // example : students[0]=s
+                    // entering second student students[1]=s//but here s acts as s1 ..
+                    Student s = new Student();
+                    // user Input
+                    System.out.println("enter the id of the student :");
+                    id = sc.nextInt();
+                    s.setId(id);
+                    // string should be string
+                    sc.nextLine(); // clear newline left by nextInt()
+                    while (true) {
                         System.out.println("enter the name of the student :");
-                        String name = sc.next();
+                        name = sc.nextLine();
+                        if (name.matches("[a-zA-z ]+")) {
+                            break;
+                        }
+                        // validation
+                        System.out.println("Alhabets only allowed !!!!");
+
+                    }
+                    s.setName(name);
+                    while (true) {
                         System.out.println("enter the Marks of the student :");
-                        int marks = sc.nextInt();
-                        // setting private variables
-                        s.setId(id);
-                        s.setName(name);
-                        s.setMarks(marks);
+                        marks = sc.nextInt();
+                        if (marks >= 0 && marks <= 100) {
+                            break;
+                        }
+                        // validation
+                        System.out.println("Marks should from 0 to 100 only!!");
+                    }
+                    s.setMarks(marks);
+                    boolean idExists = false;
+
+                    for (int i = 0; i < count; i++) {
+                        if (students[i].getId() == id) {
+                            idExists = true;
+                            break;
+                        }
+                    }
+                    if (idExists) {
+                        System.out.println("ID already exists!");
+                        System.out.println("Student not addded !");
+                    } else {
                         students[count] = s;
-                        // increasing count useful for adding new student
                         count++;
 
-                        break;
-                    case 2:
-                        // Displaying all the students ....
-                        System.out.println("Displaying All Students :");
+                        System.out.println("Student added successfully");
+                        s.display();
+                    }
+
+                    break;
+                case 2:
+                    // Displaying all the students ....
+                    System.out.println("Displaying All Students :");
+                    if (count > 0) {
+
                         for (int i = 0; i < count; i++) {
                             System.out.println(" STUDENT " + (i + 1) + " DETAILS ARE :");
                             students[i].display();
                         }
                         System.out.println("Total count of students is :" + count);
+                    } else {
+                        System.out.println("--No students are added--");
+                    }
 
-                        break;
-                    case 3:
-                        // linear search ...
-                        System.out.println("ENTER THE STUDENT ID YOU WANT SEARCH :");
-                        id = sc.nextInt();
+                    break;
+                case 3:
+                    found = false;
+                    // linear search ...
+                    System.out.println("ENTER THE STUDENT ID YOU WANT SEARCH :");
+                    id = sc.nextInt();
 
-                        for (int i = 0; i < count; i++) {
-                            if (students[i].getId() == id) {
-                                students[i].display();
-                                found = true;
-                            }
-                            if (!found) {
-                                System.out.println("Student with " + id + " is not present!!");
-                            }
+                    for (int i = 0; i < count; i++) {
+                        if (students[i].getId() == id) {
+                            students[i].display();
+                            found = true;
                         }
 
-                        break;
-                    case 4:
-                        // updating student
-                        System.out.println("ENTER THE STUDENT ID YOU WANT UPDATE :");
-                        id = sc.nextInt();
+                    }
+                    // use of flag bit
+                    if (!found) {
+                        System.out.println("Student with " + id + " is not present!!");
+                    }
 
-                        for (int i = 0; i < count; i++) {
-                            if (students[i].getId() == id) {
-                                System.out.println("what do you want to update :");
-                                System.out.println("Enter 1 for --> NAME");
-                                System.out.println("Enter 2 for --> Marks");
-                                int value1 = sc.nextInt();
-                                switch (value1) {
-                                    case 1:
-                                        System.out.println("Enter the name you want to update ");
-                                        String newName = sc.next();
-                                        students[i].setName(newName);
-                                        System.out.println("The name is updated to " + students[i].getName());
-                                        break;
-                                    case 2:
-                                        System.out.println("Enter the Marks you want to update ");
-                                        int newMarks = sc.nextInt();
-                                        students[i].setMarks(newMarks);
-                                        System.out.println("The marks is updated to " + students[i].getMarks());
-                                        break;
-                                    default:
-                                        break;
-                                }
-                                found = true;
+                    break;
+                case 4:
+                    found = false;
+                    // updating student
+                    System.out.println("ENTER THE STUDENT ID YOU WANT UPDATE :");
+                    id = sc.nextInt();
+
+                    for (int i = 0; i < count; i++) {
+                        if (students[i].getId() == id) {
+                            System.out.println("what do you want to update :");
+                            System.out.println("Enter 1 for --> NAME");
+                            System.out.println("Enter 2 for --> Marks");
+                            int value1 = sc.nextInt();
+                            switch (value1) {
+                                case 1:
+                                    System.out.println("Enter the name you want to update ");
+                                    String newName = sc.next();
+                                    students[i].setName(newName);
+                                    System.out.println("-----The name is updated Successfully----");
+                                    break;
+                                case 2:
+                                    System.out.println("Enter the Marks you want to update ");
+                                    int newMarks = sc.nextInt();
+                                    students[i].setMarks(newMarks);
+                                    System.out.println("-------The marks is updated Sucessfully------- ");
+                                    break;
+                                default:
+                                    System.out.println("error!!!");
+                                    break;
                             }
-                            if (!found) {
-                                System.out.println("Student with " + id + " is not present!!");
-                            }
+                            found = true;
                         }
+                        if (!found) {
+                            System.out.println("Student with " + id + " is not present!!");
+                        }
+                    }
 
-                        break;
-                    case 5:
-                        // Deleting student
+                    break;
+                case 5:
+                    found = false;
+                    // Deleting student
+                    System.out.println("ENTER THE STUDENT ID YOU WANT DELETE :");
+                    id = sc.nextInt();
 
-                        break;
+                    for (int i = 0; i < count; i++) {
+                        if (students[i].getId() == id) {
+                            // id found then we have delete and update indexes of next elements of array
+                            for (int j = i; j < count - 1; j++) {
+                                students[j] = students[j + 1];
+                            }
+                            // we deleting student so count will get decreased..
+                            count--;
+                            found = true;
+                            System.out.println("-----------Deleted Sucessfully--------");
+                        }
+                        if (!found) {
+                            System.out.println("Student with " + id + " is not present!!");
+                        }
+                    }
 
-                    default:
+                    break;
 
-                        System.out.println("Error !!");
-                        break;
-                }
-            } catch (Exception e) {
-                System.out.println(e);
+                default:
+
+                    System.out.println("Enter the operation mentioned Above  !!");
+                    break;
             }
-            System.out.println("Do you want to do operations again : press 1 to CONTINUE || press 0 to STOP!!!");
+            // } catch (Exception e) {
+            // System.out.println(e);
+            // }
+            System.out.println("Do you want to do use operations again : press 1 to CONTINUE || press 0 to STOP!!!");
             accessing_while = sc.nextInt();
         } while (accessing_while == 1);
         sc.close();
@@ -124,7 +189,7 @@ public class StudentManagement {
 }
 
 class Student {
-    Scanner sc = new Scanner(System.in);
+
     // using privare variable for Data security
     private int id;
     private String name;
@@ -158,8 +223,8 @@ class Student {
 
     // creating display method
     public void display() {
-        System.out.println("Student  Id : " + id);
-        System.out.println("Student  Name : " + name);
-        System.out.println("Student  Marks : " + marks);
+        System.out.println("Id : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("Marks : " + marks);
     }
 }
